8e579289c49ab70cc60e6a857005306e2d784cb0
Rodion Gatskevich <49103744+Gatskevich@users.noreply.github.com>
2021-10-26 21:48:52 +0300

    settings

---
+++ BGSales.Data/BGSStagingContext.cs
@@ -0,0 +1,41 @@
+﻿using BGSales.Domain.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+#nullable disable
+
+namespace BGSales.Data
+{
+    public partial class BGSStagingContext : IdentityDbContext<ApplicationUser>
+    {
+        public BGSStagingContext()
+        {
+        }
+
+        public BGSStagingContext(DbContextOptions<BGSStagingContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+                optionsBuilder.UseSqlServer("Server=rodion-HP-ProBook-450-G5,1433;Database=BGS-Staging;MultipleActiveResultSets=true;User id=sa;Password=Z-omby111222;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
