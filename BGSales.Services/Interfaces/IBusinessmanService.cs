ba69a78e5516fa0ab8cfea5c2505c3a2a51e4136
NiKoTiN1 <56438316+NiKoTiN1@users.noreply.github.com>
2021-11-04 02:42:36 +0300

    BGS-4-JWT-uathentication - Created Blogger and Businessman registration.

---
+++ BGSales.Services/Interfaces/IBusinessmanService.cs
@@ -0,0 +1,10 @@
+﻿using BGSales.Domain.Models;
+using System.Threading.Tasks;
+
+namespace BGSales.Services.Interfaces
+{
+    public interface IBusinessmanService
+    {
+        public Task CreateBusinessman(ApplicationUser user);
+    }
+}
