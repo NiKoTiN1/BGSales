c56d8f25bfd1f654325c89b91bb985004cbe86a9
NiKoTiN1 <56438316+NiKoTiN1@users.noreply.github.com>
2021-11-04 01:06:42 +0300

    bugfix/BGS-7-Removing-secrets - Connection string removed.
    Connection string removed from appsettings

--- BGSales.Data/BGSStagingContext.cs
+++ BGSales.Data/BGSStagingContext.cs
@@ -8,21 +8,9 @@ namespace BGSales.Data
 {
     public partial class BGSStagingContext : IdentityDbContext<ApplicationUser>
     {
-        public BGSStagingContext()
-        {
-        }
-
         public BGSStagingContext(DbContextOptions<BGSStagingContext> options)
             : base(options)
         {
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-                optionsBuilder.UseSqlServer("Server=rodion-HP-ProBook-450-G5,1433;Database=BGS-Staging;MultipleActiveResultSets=true;User id=sa;Password=Z-omby111222;");
-            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -30,7 +18,7 @@ namespace BGSales.Data
             base.OnModelCreating(modelBuilder);
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
 
-            this.OnModelCreatingPartial(modelBuilder);
+            OnModelCreatingPartial(modelBuilder);
         }
 
         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
