b49c477d0576dfc9f3ac317d70e75568a0f45ce8
NiKoTiN1 <56438316+NiKoTiN1@users.noreply.github.com>
2021-11-23 20:43:55 +0300

    BGS-67-Get-all-bloggers - Get all bloggers added

---
+++ BGSales.Web/Controllers/BloggerController.cs
@@ -0,0 +1,36 @@
+﻿using BGSales.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BGSales.Web.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("api/[controller]")]
+    public class BloggerController : Controller
+    {
+        public BloggerController(IBloggerService bloggerService)
+        {
+            _bloggerService = bloggerService;
+        }
+
+        private readonly IBloggerService _bloggerService;
+
+        [HttpGet]
+        [Route("all")]
+        public IActionResult GetAllBloggers()
+        {
+            var userIdClaim = HttpContext.User.Claims.FirstOrDefault(a => a.Type == "UserId");
+
+            if (string.IsNullOrEmpty(userIdClaim.Value))
+            {
+                return Unauthorized();
+            }
+
+            var model = _bloggerService.GetAllBloggers();
+
+            return Ok(model);
+        }
+    }
+}
