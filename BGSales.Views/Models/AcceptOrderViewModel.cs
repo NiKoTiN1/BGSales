4cbdce3145616d235f3cfb2c4ba2d96e36688b99
NiKoTiN1 <56438316+NiKoTiN1@users.noreply.github.com>
2021-11-23 20:31:02 +0300

    BGS-68-Acepted-blogger-orders - Added accept blogger logic
    (cherry picked from commit 32040edee4020a73f2b879a3e5fc4159d2d0b2a4)

---
+++ BGSales.Views/Models/AcceptOrderViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BGSales.Views.Models
+{
+    public class AcceptOrderViewModel
+    {
+        public string OrderId { get; set; }
+        public string BloggerUserId { get; set; }
+        public string BusinessmanUserId { get; set; }
+    }
+}
